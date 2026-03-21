dd4fbf6e4741e2fc99994c2464c37c64681e3af4
Kai Henrikson-Brandt <khenrik1@jh.edu>
2026-03-21 16:16:00 -0400

    adjust comment style

--- api/main.py
+++ api/main.py
@@ -6,25 +6,26 @@ import numpy as np
 import xgboost as xgb
 from transformers import AutoTokenizer, AutoModel
 
-# 1. Define the Pydantic Bouncer
+# Define Pydantic bouncer (screen for only SMILES)
 class MoleculeRequest(BaseModel):
     smiles: str
 
-# 2. The Modern "Lifespan" Manager using app.state
+# Manage API with lifespan
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("Loading models into memory...")
     
-    # Load ChemBERTa into the app's state dictionary
+    # Load ChemBERTa
     model_name = "DeepChem/ChemBERTa-77M-MTR"
     app.state.tokenizer = AutoTokenizer.from_pretrained(model_name)
     app.state.llm_extractor = AutoModel.from_pretrained(model_name)
     
+    # Configure GPU, MPS, or CPU for cross-device performance
     app.state.device = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
     app.state.llm_extractor.to(app.state.device)
     app.state.llm_extractor.eval()
     
-    # Load XGBoost into the app's state
+    # Load XGBoost
     xgb_path = "models/hybrid/xgb_hybrid.json" 
     app.state.xgb_model = xgb.XGBRegressor()
     app.state.xgb_model.load_model(xgb_path)
@@ -35,7 +36,7 @@ async def lifespan(app: FastAPI):
     
     print("Shutting down API and clearing memory...")
 
-# 3. Initialize the FastAPI app
+# Initialize the FastAPI app
 app = FastAPI(
     title="hERG Toxicity Predictor API",
     description="An API that uses a ChemBERTa + XGBoost hybrid model to predict hERG pIC50.",
@@ -43,7 +44,7 @@ app = FastAPI(
     lifespan=lifespan
 )
 
-# 4. The Prediction Endpoint (Notice we added 'request: Request')
+# Prediction
 @app.post("/predict")
 def predict_toxicity(request: Request, payload: MoleculeRequest):
     try:
@@ -57,17 +58,17 @@ def predict_toxicity(request: Request, payload: MoleculeRequest):
         )
         inputs = {k: v.to(request.app.state.device) for k, v in inputs.items()}
         
-        # Step B: Extract the 768-D [CLS] Embedding
+        # Extract the 768-D [CLS] embedding
         with torch.no_grad():
             outputs = request.app.state.llm_extractor(**inputs)
             cls_embedding = outputs.last_hidden_state[:, 0, :]
             
         embedding_array = cls_embedding.cpu().numpy()
         
-        # Step C: Predict the pIC50
+        # Predict the pIC50
         prediction = request.app.state.xgb_model.predict(embedding_array)[0]
         
-        # Step D: Return the JSON response
+        # Return the JSON response
         return {
             "smiles": payload.smiles,
             "predicted_pIC50": float(prediction),
